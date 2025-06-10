6743f77afc17631c5721a4211bb6526173465b8c
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-06-10 12:07:55 -0300

    Obtendo a data e a hora

---
+++ script.js
@@ -0,0 +1,7 @@
+//Aqui ele apenas mostra o horario atual com base na sua localização.
+console.log(new Date());
+//Colocando o "0" ele pega a primeira data que o javascript usa de referência
+console.log(new Date(0));
+
+// Exibe o número de milisegundos desde a criação da referência.
+console.log(new Date().getTime());
